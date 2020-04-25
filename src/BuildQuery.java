9b75ea8f993af1df2d08509b81ab2ada6496b176
nananananate <=>
2020-04-25 02:07:59 -0700

    finished modularizing buildQuery

--- src/BuildQuery.java
+++ src/BuildQuery.java
@@ -1,13 +1,14 @@
 // import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BuildQuery {
     private int numberOfConditions = 0;
     private int numberOfTables = 0;
 
     // SELECT ...
-    private String selectStr = "SELECT ";
+    private String selectStr = "SELECT DISTINCT ";
 
     // FROM ...
     private String fromStr = "";
@@ -23,9 +24,13 @@ public class BuildQuery {
     // maps url parameters to the MySQL table column names
     private HashMap<String, String> parametersToColumns;
 
+    private HashMap<String, String> parametersToSearchTemplates;
+    private HashMap<String, String> parametersToBrowseTemplates;
+
 
     public BuildQuery(){
         initializeParametersToColumns();
+        initializeParametersToTemplates();
     }
 
 
@@ -41,20 +46,57 @@ public class BuildQuery {
             put("director", "movies_with_rating.director");
             put("starName", "stars.name");
             put("genre", "genres.name");
+            put("year", "movies_with_ratings.year");
         }};
     }
 
-/*
-    public void addWhereConditionsFromRequest(HttpServletRequest request){
-        Enumeration<String> parameterNames = request.getParameterNames();
 
-        while(parameterNames.hasMoreElements()){
-            String name = parameterNames.nextElement();
-            String value = request.getParameter(name);
+    private void initializeParametersToTemplates(){
+        String like = "%s LIKE \"%%%s%%\"";
+        String equalsStr = "%s = \"%s\"";
+        String equalsInt = "%s = %s";
+        String beginsStr = "%s LIKE \"%s%%\"";
 
+        parametersToSearchTemplates = new HashMap<String, String>() {{
+            put("title", like);
+            put("director", like);
+            put("starName", like);
+            put("genre", equalsStr);
+            put("year", equalsInt);
+        }};
+
+        parametersToBrowseTemplates = new HashMap<String, String>() {{
+            put("title", beginsStr);
+            put("director", like);
+            put("starName", like);
+            put("genre", equalsStr);
+            put("year", equalsInt);
+        }};
+    }
+
+    // adds parameters to WHERE conditions
+    public void addParameters(Map<String, String[]> parameters){
+        // check if browsing
+        Map<String, String> templates = new HashMap<String, String>();
+        if (parameters.get("browse") != null && notEmpty(parameters.get("browse")[0]) && parameters.get("browse")[0].equals("true")) {
+            templates = parametersToBrowseTemplates;
+        }
+        // else use search
+        else {
+            templates = parametersToSearchTemplates;
+        }
+
+        for (String name: parameters.keySet()){
+            String column = parametersToColumns.get(name);
+            String template = templates.get(name);
+            if(notEmpty(template)) {
+                String value = parameters.get(name)[0];
+
+                addWhereConditions(template, column, value);
+            }
         }
     }
-*/
+
 
     private boolean notEmpty(String s){
         return (s != null && !s.equals(""));
