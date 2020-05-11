d255c8ee1334be8e28f235bfef0ca712f1b81fa0
VegetableHut <rengarfield@gmail.com>
2020-05-10 18:09:51 -0700

    Began adapting LoginFilter to also deny access to dashboard unless employee. Issue: Infinite filter loop

--- src/LoginFilter.java
+++ src/LoginFilter.java
@@ -10,7 +10,9 @@ import java.util.ArrayList;
  */
 @WebFilter(filterName = "LoginFilter", urlPatterns = "/*")
 public class LoginFilter implements Filter {
+
     private final ArrayList<String> allowedURIs = new ArrayList<>();
+    private final ArrayList<String> protectedURIs = new ArrayList<>();
 
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
@@ -23,17 +25,32 @@ public class LoginFilter implements Filter {
         System.out.println("LoginFilter: " + httpRequest.getRequestURI());
 
         // Check if this URL is allowed to access without logging in
-        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI())) {
+        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI()) && !this.requiresEmployee(httpRequest.getRequestURI())) {
             // Keep default action: pass along the filter chain
+            System.out.println("filter not needed for this page");
             chain.doFilter(request, response);
             return;
         }
 
         // Redirect to login page if the "user" attribute doesn't exist in session
         if (httpRequest.getSession().getAttribute("user") == null) {
+            System.out.println("Login filter activated, redirection sent");
             httpResponse.sendRedirect("login.html");
+            return;
+        } else if(this.requiresEmployee(httpRequest.getRequestURI())) {
+            if (! httpRequest.getSession().getAttribute("role").equals("employee")) {
+                System.out.println("Dashboard filter activated, redirection sent");
+                httpResponse.sendRedirect("employeeAccess.html");
+                return;
+            } else {
+                System.out.println("***************** BALONEY ************");
+                chain.doFilter(request, response);
+                return;
+            }
         } else {
+            System.out.println("***************** MACARONI ************");
             chain.doFilter(request, response);
+            return;
         }
     }
 
@@ -46,10 +63,16 @@ public class LoginFilter implements Filter {
         return allowedURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
     }
 
+    private boolean requiresEmployee(String requestURI){
+        return protectedURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
+    }
+
     public void init(FilterConfig fConfig) {
         allowedURIs.add("login.html");
         allowedURIs.add("login.js");
         allowedURIs.add("api/login");
+        protectedURIs.add("_dashboard.html");
+        protectedURIs.add("dashboard.js");
     }
 
     public void destroy() {
