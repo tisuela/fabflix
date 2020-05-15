eb0f09caa757e89d70c519937ab858249c5329a1
VegetableHut <rengarfield@gmail.com>
2020-05-14 23:14:00 -0700

    Changed the returning json to have less information on return for operations like autocomplete

--- src/MoviesServlet.java
+++ src/MoviesServlet.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import java.util.Set;
 
 
+
 // Declaring a WebServlet called MoviesServlet, which maps to url "/api/movies"
 @WebServlet(name = "MoviesServlet", urlPatterns = "/api/movies")
 public class MoviesServlet extends HttpServlet {
@@ -30,6 +31,9 @@ public class MoviesServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
 
+    private boolean notEmpty(String s){
+        return (s != null && !s.equals(""));
+    }
 
     // Build query for MYSQL from request parameters
     private MyQuery buildQuery(HttpServletRequest request, Connection dbcon){
@@ -102,6 +106,9 @@ public class MoviesServlet extends HttpServlet {
 
             int resultSetCount = 0;
 
+            // Main JSON to be returned
+            JsonObject jsonObject = new JsonObject();
+            // JSON Array with all movie entries
             JsonArray jsonArray = new JsonArray();
 
             // Iterate through each row of rs
@@ -113,72 +120,77 @@ public class MoviesServlet extends HttpServlet {
                 String movie_director = rs.getString("director");
                 String movie_rating = rs.getString("rating");
 
-                // Movie stars and genres will be stored as arrays
-                JsonArray jsonStars = new JsonArray();
-                JsonArray jsonGenres = new JsonArray();
+                String autocomplete = request.getParameter("autocomplete");
 
-                // additional queries for genres and stars
-                try {
-                    // Get list of first three genres
-                    MyQuery genreQuery = new MyQuery(dbcon);
-                    genreQuery.setSelectStr("genres.name");
-                    genreQuery.addFromTables("genres JOIN genres_in_movies ON (genres.id = genreId AND movieId = ?)", movie_id);
-                    genreQuery.append("ORDER BY genres.name LIMIT ?", "3", "int");
-                    ResultSet genreSet = genreQuery.execute();
+                // search param is on? then do this stuff, otherwise skip
+                if (!this.notEmpty(autocomplete)) {
+                    // Movie stars and genres will be stored as arrays
+                    JsonArray jsonStars = new JsonArray();
+                    JsonArray jsonGenres = new JsonArray();
 
-                    // get list of first three stars
+                    // additional queries for genres and stars
+                    try {
+                        // Get list of first three genres
+                        MyQuery genreQuery = new MyQuery(dbcon);
+                        genreQuery.setSelectStr("genres.name");
+                        genreQuery.addFromTables("genres JOIN genres_in_movies ON (genres.id = genreId AND movieId = ?)", movie_id);
+                        genreQuery.append("ORDER BY genres.name LIMIT ?", "3", "int");
+                        ResultSet genreSet = genreQuery.execute();
 
-                    // Build Stars query
-                    MyQuery starsQuery = new MyQuery(dbcon,"SELECT *, COUNT(*) as totalMovies");
+                        // get list of first three stars
 
-                    // first get the stars in the movie
-                    starsQuery.addFromTables("stars JOIN stars_in_movies as in_movie ON (stars.id = starId and movieId = ?)", movie_id);
+                        // Build Stars query
+                        MyQuery starsQuery = new MyQuery(dbcon, "SELECT *, COUNT(*) as totalMovies");
 
-                    // Join again with allstars to get all the movies the stars in THIS MOVIE starred in
-                    starsQuery.addFromTables("JOIN stars_in_movies as all_stars ON (in_movie.starId = all_stars.starId)");
-                    starsQuery.append("GROUP BY all_stars.starId ORDER BY totalMovies DESC, stars.name ASC LIMIT ?", 3);
-                    ResultSet starsSet = starsQuery.execute();
+                        // first get the stars in the movie
+                        starsQuery.addFromTables("stars JOIN stars_in_movies as in_movie ON (stars.id = starId and movieId = ?)", movie_id);
 
-                    // assemble genre list (as a JSON object)
-                    while (genreSet.next()){
-                        JsonObject jsonGenre = new JsonObject();
-                        String genre_name = genreSet.getString("name");
-                        jsonGenre.addProperty("genre_name", genre_name);
+                        // Join again with allstars to get all the movies the stars in THIS MOVIE starred in
+                        starsQuery.addFromTables("JOIN stars_in_movies as all_stars ON (in_movie.starId = all_stars.starId)");
+                        starsQuery.append("GROUP BY all_stars.starId ORDER BY totalMovies DESC, stars.name ASC LIMIT ?", 3);
+                        ResultSet starsSet = starsQuery.execute();
 
-                        // add to the JSON array of stars
-                        jsonGenres.add(jsonGenre);
+                        // assemble genre list (as a JSON object)
+                        while (genreSet.next()) {
+                            JsonObject jsonGenre = new JsonObject();
+                            String genre_name = genreSet.getString("name");
+                            jsonGenre.addProperty("genre_name", genre_name);
+
+                            // add to the JSON array of stars
+                            jsonGenres.add(jsonGenre);
+                        }
+
+                        // stars list (as JSON object)
+                        while (starsSet.next()) {
+                            JsonObject jsonStar = new JsonObject();
+                            String star_id = starsSet.getString("starId");
+                            String star_name = starsSet.getString("name");
+                            jsonStar.addProperty("star_id", star_id);
+                            jsonStar.addProperty("star_name", star_name);
+
+                            // add to the JSON array of stars
+                            jsonStars.add(jsonStar);
+                        }
+
+                        // free resources
+                        genreQuery.close();
+                        starsQuery.close();
+
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
                     }
 
-                    // stars list (as JSON object)
-                    while(starsSet.next()){
-                        JsonObject jsonStar = new JsonObject();
-                        String star_id = starsSet.getString("starId");
-                        String star_name = starsSet.getString("name");
-                        jsonStar.addProperty("star_id", star_id);
-                        jsonStar.addProperty("star_name", star_name);
-
-                        // add to the JSON array of stars
-                        jsonStars.add(jsonStar);
-                    }
-
-                    // free resources
-                    genreQuery.close(); starsQuery.close();
-
-
-                } catch (Exception e){
-                    System.out.println(e.getMessage());
+                    jsonObject.add("movie_genres", jsonGenres);
+                    jsonObject.add("movie_stars", jsonStars);
                 }
 
-
                 // Create a JsonObject based on the data we retrieve from rs
-                JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("movie_id", movie_id);
                 jsonObject.addProperty("movie_title", movie_title);
                 jsonObject.addProperty("movie_year", movie_year);
                 jsonObject.addProperty("movie_director", movie_director);
                 jsonObject.addProperty("movie_rating", movie_rating);
-                jsonObject.add("movie_genres", jsonGenres);
-                jsonObject.add("movie_stars", jsonStars);
                 jsonArray.add(jsonObject);
             }
 
