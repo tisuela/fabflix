d08e829ce9f803f430443b081a8d33f0ee05c94e
VegetableHut <rengarfield@gmail.com>
2020-05-25 13:38:49 -0700

    Updated constant URL for Project 4 Demo. Final Stable Commit

--- Mobile/app/src/main/java/edu/uci/ics/fabflixmobile/Constants.java
+++ Mobile/app/src/main/java/edu/uci/ics/fabflixmobile/Constants.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.fabflixmobile;
 
 public class Constants {
-    public static final String url = "https://https://ec2-54-198-73-150.compute-1.amazonaws.com:8443/fabflix/api/";
+    public static final String url = "https://ec2-54-198-73-150.compute-1.amazonaws.com:8443/fabflix/api/";
 }
